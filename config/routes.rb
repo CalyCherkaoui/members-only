df39ed37e0d499b6e21158b64aaa96ef1ac0397f
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-26 14:36:21 -0500

    Updating Redme and fixing rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts
+  resources :posts, only: %i[new create index]
 
   root "posts#index"
 end
