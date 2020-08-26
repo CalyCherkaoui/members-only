867458dbd3b7a5aac13bf36af4ca91ceceaaca17
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-08-26 22:48:48 +0300

    Fix more style lint errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts, only: %i[new create index]
+  resources :posts, only: %i[new create index destroy]
 
   root "posts#index"
 end
