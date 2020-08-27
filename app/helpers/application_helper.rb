ee2c99a6c35a6eaeaea2fd8bdbb8cb4cbfa831e3
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-27 09:53:35 -0500

    Creating login helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def my_post?(post)
     user_signed_in? && post.user_id == current_user.id
   end
+
+  def login_helper
+    if user_signed_in?
+      welcome = "Hi"+ current_user.name + "welcome to MembersOnly app!"
+      content_tag(:h3, welcome) do 
+        content_tag(:small) do
+          link_to "Sign-out", destroy_user_session_url, method: :delete
+        end 
+      end
+    else
+      link_to 'Sign-up', new_user_registration_path
+      content_tag(:br)
+      link_to 'Log-in', new_user_session_path
+    end
+  end
 end
