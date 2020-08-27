d9e617c4f094be09f2269fa40a928b9b6d08a66a
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-27 10:30:25 -0500

    Updating flash and log-in helpres

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,19 @@ module ApplicationHelper
       link = ('<small>' + link_to('Sign-out', destroy_user_session_url, method: :delete) + '</small><h3>')
       (welcome + link).html_safe
     else
-      link_to 'Sign-up', new_user_registration_path
-      content_tag(:br)
-      link_to 'Log-in', new_user_session_path
+      link1 = link_to('Sign-up', new_user_registration_path)
+      link2 = link_to('Log-in', new_user_session_path)
+      (link1 + '<br>'.html_safe + link2)
     end
+  end
+
+  def flash_helper
+     if flash[:notice]
+      "<p class=\"notice\"> #{notice} </p>".html_safe
+     end
+
+     if flash[:alert]
+      "<p class=\"alert\"> #{alert} </p>".html_safe
+     end
   end
 end
