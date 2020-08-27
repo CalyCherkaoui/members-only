815ea57b5674b3244908d0fd656332249ec346d1
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-27 10:15:01 -0500

    Updating post helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,9 @@
 module ApplicationHelper
-
   def login_helper
     if user_signed_in?
-      welcome = "Hi"+ current_user.name + "welcome to MembersOnly app!"
-      content_tag(:h3, welcome) do 
-        content_tag(:small) do
-          link_to "Sign-out", destroy_user_session_url, method: :delete
-        end 
-      end
+      welcome = '<h3>Hi ' + current_user.name + '! welcome to MembersOnly app!'
+      link = ('<small>' + link_to('Sign-out', destroy_user_session_url, method: :delete) + '</small><h3>')
+      (welcome + link).html_safe
     else
       link_to 'Sign-up', new_user_registration_path
       content_tag(:br)
